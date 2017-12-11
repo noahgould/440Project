07baad5de17c1e066eacd79c7f45bfaf28d6198a
noahgould <gould.noah@gmail.com>
2017-12-11 15:30:25 -0600

    Plots all data points at once.

--- StatInterpolater.m
+++ StatInterpolater.m
@@ -1,50 +1,56 @@
-function playerData = StatInterpolater(playerName)
+function StatInterpolater(playerName)
     % Import data from CSV file
     fileName = strcat('Data\', strrep(playerName, ' ', '_'));
     fileName = strcat(fileName, '.csv');
     playerData = csvread(fileName,1,1);
 
+    columnNames = {'FG Percentage', '3P Percentage', 'FT Percentage', 'Assists Per Game', 'Points Per Game'};
+    columnNames = string(columnNames);
+    
     % Set year range for data
     years = 2008:2014;
     polyYears = years;
     polyYears(4) = [];
 
-    % Get FG percentage data
-    actFg = playerData(:,1)';
-    interpolateFg = actFg;
-    interpolateFg(4) = [];
+    for column = 1:5
+        % Get data
+        actValues = playerData(:,column)';
+        interpolateValues = actValues;
+        interpolateValues(4) = [];
 
-    % Calculate Lagrange polynomial for FG percentage
-    fgPolynomial = Lagrange(polyYears, interpolateFg);
-    piecewise = LinearInterpolation(polyYears, interpolateFg);
+        % Calculate Lagrange polynomial for FG percentage
+        fgPolynomial = Lagrange(polyYears, interpolateValues);
+        piecewise = LinearInterpolation(polyYears, interpolateValues);
 
 
-    % Get data points from Lagrange polynomial for FG percentage
-    syms x
-    lagrangeFG = zeros(1,7);
-    piecewiseFG = zeros(1,7);
-    for i = 1:7
-        lagrangeFG(i) = subs(fgPolynomial, x, years(i));
-    end
+        % Get data points from Lagrange polynomial for FG percentage
+        syms x
+        lagrangeValues = zeros(1,7);
+        piecewiseValues = zeros(1,7);
+        for i = 1:7
+            lagrangeValues(i) = subs(fgPolynomial, x, years(i));
+        end
 
-    for i = 1:length(years)
-        for j = 1:length(piecewise) - 1
-            if years(i) >= piecewise(j,2) && years(i) < piecewise(j+1, 2)
-                pieceToUse = piecewise(j,1);
+        for i = 1:length(years)
+            for j = 1:length(piecewise) - 1
+                if years(i) >= piecewise(j,2) && years(i) < piecewise(j+1, 2)
+                    pieceToUse = piecewise(j,1);
+                end
             end
-        end
-        if i == 7
-            pieceToUse = piecewise(length(piecewise), 1);
+            if i == 7
+                pieceToUse = piecewise(length(piecewise), 1);
+            end
+
+            piecewiseValues(i) = subs(pieceToUse, x, years(i));
         end
 
-        piecewiseFG(i) = subs(pieceToUse, x, years(i));
+        % Plot FG percentage Lagrange polynomial against actual data
+        subplot(3,2,column)
+        plot(years,actValues,'o',years,lagrangeValues,'-', years,piecewiseValues, '-.');
+        title(strcat(playerName, {' '}, columnNames(column), ' from 2008-2014'));
+        xlabel('Year');
+        ylabel(columnNames(column));
+        legend('Actual', 'Lagrange', 'Piecewise Linear');
+        legend('Location', 'southwest');
     end
-
-    % Plot FG percentage Lagrange polynomial against actual data
-    plot(years,actFg,'o',years,lagrangeFG,'-', years,piecewiseFG, '-.');
-    title(strcat(playerName, ' FG Percentage from 2008-2014'));
-    xlabel('Year');
-    ylabel('FG Percentage');
-    legend('Actual', 'Lagrange', 'Piecewise Linear');
-    legend('Location', 'southwest');
 end
